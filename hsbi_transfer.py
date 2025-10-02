0551c1d5686432bcd51671197e8f81aa0b959e9a
Michael Garcia <thecrazygm@gmail.com>
2025-10-02 12:20:40 -0400

    fix: use consistent sbi account name throughout transfer script

--- hsbi_transfer.py
+++ hsbi_transfer.py
@@ -33,7 +33,7 @@ def run():
     accountTrx = {}
     for account in accounts:
         if account == "steembasicincome":
-            accountTrx[account] = AccountTrx(db, "sbi")
+            accountTrx["sbi"] = AccountTrx(db, "sbi")
         else:
             accountTrx[account] = AccountTrx(db, account)
 
@@ -84,6 +84,8 @@ def run():
         # stop_index = formatTimeString("2018-07-21T23:46:09")
 
         for account_name in accounts:
+            if account_name == "steembasicincome":
+                account_name = "sbi"
             account_trx = accountTrx[account_name]
             parse_vesting = account_name == "steembasicincome"
             account = Account(account_name, blockchain_instance=hv)
