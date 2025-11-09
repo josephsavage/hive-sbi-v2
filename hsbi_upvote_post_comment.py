d7ef2f903bfe18ccc6121e6de84e75277f26f195
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2025-11-09 03:34:25 -0800

    Update hsbi_upvote_post_comment.py
    printed the wrong list

--- hsbi_upvote_post_comment.py
+++ hsbi_upvote_post_comment.py
@@ -122,7 +122,7 @@ def run():
             eligible_authors.append(author)
         except Exception:
             continue
-    print("Eligible voters:", len(eligible_voters))
+    print("Eligible authors:", len(eligible_authors))
 
     # --- start: sequence posts by member balance_rshares instead of creation time ---
     unvoted = postTrx.get_unvoted_post()  # dict keyed by authorperm
