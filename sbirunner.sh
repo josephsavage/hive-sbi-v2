8e538953b21c646a873070e559217b645304c1d4
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2025-11-18 12:05:43 -0800

    Update sbirunner.sh

--- sbirunner.sh
+++ sbirunner.sh
@@ -7,8 +7,8 @@ python3 -u /root/steembasicincome/hsbi_check_delegation.py
 python3 -u /root/steembasicincome/hsbi_token_snapshot.py
 python3 -u /root/steembasicincome/hsbi_claim_rewards.py
 python3 -u /root/steembasicincome/hsbi_update_member_db.py
-python3 -u /root/steembasicincome/hsbi_manage_accrual.py
 python3 -u /root/steembasicincome/hsbi_store_member_hist.py
 python3 -u /root/steembasicincome/hsbi_upvote_post_comment.py
 python3 -u /root/steembasicincome/hsbi_stream_post_comment.py
+python3 -u /root/steembasicincome/hsbi_manage_accrual.py
 #python3 -u /root/steembasicincome/hsbi_reset_rshares.py
